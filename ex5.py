6d78e541260616cbb9a72101bb2aa42ac6322b99
edgarrp325 <111950103+edgarrp325@users.noreply.github.com>
2023-04-12 11:28:54 -0600

    Add files via upload

---
+++ ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Edgar Eduardo Ruiz Pérez'
+my_age = 22
+my_height = 1.70 #metros
+my_weight = 100 #kilos
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} meters tall.")
+print(f"He's {my_weight} kilograms heavy.")
+print(f"Actually tha's not too heavy")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+# this line is tricky, try to get it exactly right
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
